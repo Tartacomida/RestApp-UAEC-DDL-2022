d9089d8333248e1598c9dee097fa53c00cd88641
Tartacomida <utp0142383@alumno.utpuebla.edu.mx>
2022-06-07 11:54:34 -0500

    modificado layout del boton y lista de mensajes agregada

--- app/src/main/java/com/example/restapp/MainActivity.java
+++ app/src/main/java/com/example/restapp/MainActivity.java
@@ -2,22 +2,37 @@ package com.example.restapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.restapp.databinding.ActivityMainBinding;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends Activity {
 
     private TextView mTextView;
+    private Button nButton;
     private ActivityMainBinding binding;
+    private final List<String> mensajes = Arrays.asList(
+            "Hola Como estas?",
+            "Habia una vez un perro",
+            "Se cayo y se pego",
+            "jsjsjsjs",
+            "alla en la fuente",
+            "habia un chorrito",
+            "se hacia grandote",
+            " y se hacia chiquito"
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        mTextView = findViewById(R.id.Saludo);
+        nButton = findViewById(R.id.btn1);
 
-        mTextView = binding.text;
+        mTextView.setText(mensajes.get(1));
     }
 }
